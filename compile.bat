ba61677e535a048521f579632177d30c05091781
Alex Brochu <alexbrochu1@gmail.com>
2022-11-10 12:17:39 -0500

    [SETUP] Better script to compile with less output + parallel

--- compile.bat
+++ compile.bat
@@ -4,4 +4,4 @@ set "buildfolder=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\M
 set "execpath=%buildfolder%\MSBuild.exe"
 
 echo %execpath%
-"%execpath%" ./build/ModelViewerDx12.sln
+"%execpath%" -nologo -m ./build/ModelViewerDx12.sln
