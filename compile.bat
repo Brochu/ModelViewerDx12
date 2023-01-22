f6f4a56f696594b8fbe72697967e0a1fc1b9bba6
Alex Brochu <alexbrochu1@gmail.com>
2023-01-22 15:35:38 -0500

    [FIX] updated the MSBuild version, VS2022 community now

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "buildfolder=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin"
+set "buildfolder=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin"
 set "execpath=%buildfolder%\MSBuild.exe"
 
 echo %execpath%
