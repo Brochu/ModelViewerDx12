c5771dff114c953331d0929c9a568e5348a6396a
Alex Brochu <alexbrochu1@gmail.com>
2023-02-09 10:16:58 -0500

    [FIX] Convert indents to spaces

--- shaders/shaders.hlsl
+++ shaders/shaders.hlsl
@@ -1,89 +1,35 @@
-#ifdef D3D12_SAMPLE_BASIC
-struct VertexShaderOutput
-{
-	float4 position : SV_POSITION;
-	float2 uv : TEXCOORD;
-};
-
-VertexShaderOutput VS_main(
-	float4 position : POSITION,
-	float2 uv : TEXCOORD)
-{
-	VertexShaderOutput output;
-
-	output.position = position;
-	output.uv = uv;
-
-	return output;
-}
-
-float4 PS_main (float4 position : SV_POSITION,
-				float2 uv : TEXCOORD) : SV_TARGET
-{
-	return float4(uv, 0, 1);
-}
-#elif D3D12_SAMPLE_CONSTANT_BUFFER
+#ifdef D3D12_SAMPLE_TEXTURE
 cbuffer PerFrameConstants : register (b0)
 {
-	float4 scale;
+    float4 scale;
 }
 
 struct VertexShaderOutput
 {
-	float4 position : SV_POSITION;
-	float2 uv : TEXCOORD;
+    float4 position : SV_POSITION;
+    float2 uv : TEXCOORD;
 };
 
 VertexShaderOutput VS_main(
-	float4 position : POSITION,
-	float2 uv : TEXCOORD)
+    float4 position : POSITION,
+    float2 uv : TEXCOORD)
 {
-	VertexShaderOutput output;
+    VertexShaderOutput output;
 
-	output.position = position;
-	output.position.xy *= scale.x;
-	output.uv = uv;
+    output.position = position;
+    output.position.xy *= scale.x;
+    output.uv = uv;
 
-	return output;
-}
-
-float4 PS_main (float4 position : SV_POSITION,
-				float2 uv : TEXCOORD) : SV_TARGET
-{
-	return float4(uv, 0, 1);
-}
-#elif D3D12_SAMPLE_TEXTURE
-cbuffer PerFrameConstants : register (b0)
-{
-	float4 scale;
-}
-
-struct VertexShaderOutput
-{
-	float4 position : SV_POSITION;
-	float2 uv : TEXCOORD;
-};
-
-VertexShaderOutput VS_main(
-	float4 position : POSITION,
-	float2 uv : TEXCOORD)
-{
-	VertexShaderOutput output;
-
-	output.position = position;
-	output.position.xy *= scale.x;
-	output.uv = uv;
-
-	return output;
+    return output;
 }
 
 Texture2D<float4> anteruTexture : register(t0);
 SamplerState texureSampler      : register(s0);
 
 float4 PS_main (float4 position : SV_POSITION,
-				float2 uv : TEXCOORD) : SV_TARGET
+                float2 uv : TEXCOORD) : SV_TARGET
 {
-	float4 tint = float4(scale.y, scale.z, scale.w, 1.0);
-	return tint * anteruTexture.Sample (texureSampler, uv);
+    float4 tint = float4(scale.y, scale.z, scale.w, 1.0);
+    return tint * anteruTexture.Sample (texureSampler, uv);
 }
 #endif
