809e9c4ccae8f80e7b6dc856c653f4560f7f804b
Alex Brochu <alexbrochu1@gmail.com>
2023-02-10 21:26:53 -0500

    [DEBUG] Removed debug messages, no need to debug draw calls for now

--- src/D3D12TexturedQuad.cpp
+++ src/D3D12TexturedQuad.cpp
@@ -20,12 +20,10 @@
 // THE SOFTWARE.
 //
 
-#define NOMINMAX
 #include "D3D12TexturedQuad.h"
 #include "ImageIO.h"
 #include "RubyTexture.h"
 #include "Utility.h"
-#include "Tracy.hpp"
 
 #include <cmath>
 #include <d3dx12.h>
@@ -228,20 +226,6 @@ void D3D12TexturedQuad::CreateMeshBuffers (ID3D12GraphicsCommandList* uploadComm
     std::vector<unsigned int> indices;
 
     draws_ = ExtractAiScene(path.c_str(), vertices, indices);
-    for (size_t i = 0; i < draws_.numDraws; i++) {
-        std::string out("Draw call - ");
-        out.append("Vertex Offset : ");
-        out.append(std::to_string(draws_.vertexOffsets[i]));
-        out.append(", ");
-
-        out.append("Index Offset : ");
-        out.append(std::to_string(draws_.indexOffsets[i]));
-        out.append(", ");
-
-        out.append("Index Count : ");
-        out.append(std::to_string(draws_.indexCounts[i]));
-        TracyMessage(out.c_str(), out.size());
-    }
 
     unsigned int vertexCount = (UINT) vertices.size();
     unsigned int indexCount = (UINT) indices.size();
