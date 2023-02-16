d1b8f36c64b1d1ae5613b534c441c861c11137ae
Alex Brochu <alexbrochu1@gmail.com>
2023-02-16 17:30:52 -0500

    [WIP] Upload texture data, start with committed resources for now

--- src/D3D12TexturedQuad.cpp
+++ src/D3D12TexturedQuad.cpp
@@ -55,53 +55,68 @@ D3D12TexturedQuad::D3D12TexturedQuad(int modelOverride) {
 ///////////////////////////////////////////////////////////////////////////////
 void D3D12TexturedQuad::CreateTexture (ID3D12GraphicsCommandList * uploadCommandList)
 {
-    //TODO: Use this as a base to upload texture data from assimp import
-    int width = 0, height = 0;
-
-    imageData_ = LoadImageFromMemory (RubyTexture, sizeof (RubyTexture),
-        1 /* tight row packing */, &width, &height);
+    size_t uploadSize = 0;
+    std::vector<std::uint8_t> data;
 
     static const auto defaultHeapProperties = CD3DX12_HEAP_PROPERTIES (D3D12_HEAP_TYPE_DEFAULT);
-    const auto resourceDesc = CD3DX12_RESOURCE_DESC::Tex2D (DXGI_FORMAT_R8G8B8A8_UNORM_SRGB, width, height, 1, 1);
 
-    device_->CreateCommittedResource (&defaultHeapProperties,
-        D3D12_HEAP_FLAG_CREATE_NOT_ZEROED,
-        &resourceDesc,
-        D3D12_RESOURCE_STATE_COPY_DEST,
-        nullptr,
-        IID_PPV_ARGS (&image_));
+    for (size_t i = 0; i < materials_.size(); i++) {
+        //TODO: This is pretty ugly, why do we have textures with empty names?
+        image_.emplace_back();
+        uploadImage_.emplace_back();
+        
+        Material m = materials_[i];
+        if (m.textureName.size() <= 0) continue;
 
-    static const auto uploadHeapProperties = CD3DX12_HEAP_PROPERTIES (D3D12_HEAP_TYPE_UPLOAD);
-    const auto uploadBufferSize = GetRequiredIntermediateSize (image_.Get (), 0, 1);
-    const auto uploadBufferDesc = CD3DX12_RESOURCE_DESC::Buffer (uploadBufferSize);
+        int width = 0, height = 0;
+        std::string path = "data/models/" + models_[modelIndex_] + "/" + m.textureName;
+        std::vector<std::uint8_t> imgdata = LoadImageFromFile(path.c_str(), 1, &width, &height);
 
-    device_->CreateCommittedResource (&uploadHeapProperties,
-        D3D12_HEAP_FLAG_NONE,
-        &uploadBufferDesc,
-        D3D12_RESOURCE_STATE_GENERIC_READ,
-        nullptr,
-        IID_PPV_ARGS (&uploadImage_));
+        std::string out(path);
+        out.append("; size = ");
+        out.append(std::to_string(imgdata.size()));
+        TracyMessage(out.c_str(), out.size());
 
-    D3D12_SUBRESOURCE_DATA srcData;
-    srcData.pData = imageData_.data ();
-    srcData.RowPitch = width * 4;
-    srcData.SlicePitch = width * height * 4;
+        const auto resourceDesc = CD3DX12_RESOURCE_DESC::Tex2D ( DXGI_FORMAT_R8G8B8A8_UNORM_SRGB, width, height, 1, 1 );
+        device_->CreateCommittedResource (&defaultHeapProperties,
+            D3D12_HEAP_FLAG_CREATE_NOT_ZEROED,
+            &resourceDesc,
+            D3D12_RESOURCE_STATE_COPY_DEST,
+            nullptr,
+            IID_PPV_ARGS (&image_[i]));
 
-    UpdateSubresources (uploadCommandList, image_.Get (), uploadImage_.Get (), 0, 0, 1, &srcData);
-    const auto transition = CD3DX12_RESOURCE_BARRIER::Transition (image_.Get (),
-        D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
-    uploadCommandList->ResourceBarrier (1, &transition);
+        static const auto uploadHeapProperties = CD3DX12_HEAP_PROPERTIES (D3D12_HEAP_TYPE_UPLOAD);
+        const auto uploadBufferSize = GetRequiredIntermediateSize (image_[i].Get (), 0, 1);
+        const auto uploadBufferDesc = CD3DX12_RESOURCE_DESC::Buffer (uploadBufferSize);
 
-    D3D12_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc = {};
-    shaderResourceViewDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
-    shaderResourceViewDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-    shaderResourceViewDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
-    shaderResourceViewDesc.Texture2D.MipLevels = 1;
-    shaderResourceViewDesc.Texture2D.MostDetailedMip = 0;
-    shaderResourceViewDesc.Texture2D.ResourceMinLODClamp = 0.0f;
+        device_->CreateCommittedResource (&uploadHeapProperties,
+            D3D12_HEAP_FLAG_NONE,
+            &uploadBufferDesc,
+            D3D12_RESOURCE_STATE_GENERIC_READ,
+            nullptr,
+            IID_PPV_ARGS (&uploadImage_[i]));
 
-    device_->CreateShaderResourceView (image_.Get (), &shaderResourceViewDesc,
-        srvDescriptorHeap_->GetCPUDescriptorHandleForHeapStart ());
+        D3D12_SUBRESOURCE_DATA srcData;
+        srcData.pData = imgdata.data ();
+        srcData.RowPitch = width * 4;
+        srcData.SlicePitch = width * height * 4;
+
+        UpdateSubresources (uploadCommandList, image_[i].Get (), uploadImage_[i].Get (), 0, 0, 1, &srcData);
+        const auto transition = CD3DX12_RESOURCE_BARRIER::Transition (image_[i].Get (),
+            D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+        uploadCommandList->ResourceBarrier (1, &transition);
+
+        D3D12_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc = {};
+        shaderResourceViewDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+        shaderResourceViewDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+        shaderResourceViewDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+        shaderResourceViewDesc.Texture2D.MipLevels = 1;
+        shaderResourceViewDesc.Texture2D.MostDetailedMip = 0;
+        shaderResourceViewDesc.Texture2D.ResourceMinLODClamp = 0.0f;
+
+        device_->CreateShaderResourceView (image_[i].Get (), &shaderResourceViewDesc,
+            srvDescriptorHeap_->GetCPUDescriptorHandleForHeapStart ());
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
