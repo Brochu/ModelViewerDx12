a097373a1ce3684c267082a3c87e925902e03be5
Brochu <alexbrochu1@gmail.com>
2023-04-18 11:49:25 -0400

    [CONFIG] Split parse model entry logic to separate function
    This will make it easier to handle multiple submeshes later. Will
    probably test multiple meshes with Odessey worlds

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,6 +1,18 @@
 #include "Config.h"
 #include <fstream>
 
+AMD::ModelEntry ParseEntry(const std::string& line) {
+    const auto idx = line.find('|');
+    const std::string folder = line.substr(0, idx);
+    const std::string file = line.substr(idx + 1);
+
+    //TODO: Handle sub models found in the selected model folder
+    // Return an array of all scenes to combine
+    // Find a way to find a list of offsets for each sub models
+
+    return { folder, {file} };
+};
+
 namespace AMD {
 const char *configFile = "config.ini";
 
@@ -10,21 +22,13 @@ Config ParseConfig() {
     std::string line;
 
     while (std::getline(fs, line)) {
-        if (line[0] == '[' && line.find("models") != std::string::npos) {
+        if (strcmp(line.c_str(), "[models]") == 0) {
             // Parsing possible models to render
             std::string line;
 
             while (std::getline(fs, line)) {
                 if (line.size() == 0) break;
-
-                const auto idx = line.find('|');
-                const std::string folder = line.substr(0, idx);
-                const std::string file = line.substr(idx + 1);
-
-                c.models.push_back({folder, {file} });
-                //TODO: Handle sub models found in the selected model folder
-                // Return an array of all scenes to combine
-                // Find a way to find a list of offsets for each sub models
+                c.models.push_back(ParseEntry(line));
             }
         }
     }
