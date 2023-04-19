8a8398ec2014541f5bf9e6fdb5c4c57515beef18
Brochu <alexbrochu1@gmail.com>
2023-04-19 10:46:31 -0400

    [WIP][CONFIG] Added definition for a model group (game)

--- include/Config.h
+++ include/Config.h
@@ -5,12 +5,18 @@
 
 namespace AMD{
 
+struct GroupEntry {
+    std::string name;
+    std::vector<size_t> modelrefs;
+};
+
 struct ModelEntry {
     std::string folder;
     std::vector<std::string> files;
 };
 
 struct Config {
+    std::vector<GroupEntry> groups;
     std::vector<ModelEntry> models;
 };
 
