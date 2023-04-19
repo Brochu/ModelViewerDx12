0c1e0d1d6c98b0df24a369ba2ec0523e87c53c69
Brochu <alexbrochu1@gmail.com>
2023-04-19 11:23:20 -0400

    [WIP][CONFIG] Updated config loading&parsing to handle model groups

--- src/Config.cpp
+++ src/Config.cpp
@@ -13,6 +13,35 @@ AMD::ModelEntry ParseEntry(const std::string& line) {
     return { folder, {file} };
 };
 
+AMD::GroupEntry ParseGroup(const std::string& line, const std::vector<std::string>& models, AMD::Config& c) {
+    std::vector<size_t> refs;
+    for (const auto& m : models) {
+        refs.emplace_back(c.models.size());
+        c.models.push_back(ParseEntry(m));
+    }
+
+    return { line, refs };
+}
+
+void ParseModelSection(std::fstream& fs, AMD::Config& c) {
+    // Parsing possible models to render
+    std::string groupLine;
+
+    while (std::getline(fs, groupLine)) {
+        if (groupLine.size() == 0) break;
+
+        std::vector<std::string> modelLines{};
+        std::string line;
+
+        while (std::getline(fs, line)) {
+            if (line.size() == 0 || line[0] != '-') break;
+            modelLines.emplace_back(line.substr(1));
+        }
+
+        c.groups.push_back(ParseGroup(groupLine, modelLines, c));
+    }
+}
+
 namespace AMD {
 const char *configFile = "config.ini";
 
@@ -23,13 +52,7 @@ Config ParseConfig() {
 
     while (std::getline(fs, line)) {
         if (strcmp(line.c_str(), "[models]") == 0) {
-            // Parsing possible models to render
-            std::string line;
-
-            while (std::getline(fs, line)) {
-                if (line.size() == 0) break;
-                c.models.push_back(ParseEntry(line));
-            }
+            ParseModelSection(fs, c);
         }
     }
 
