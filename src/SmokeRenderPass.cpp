3c636c415cddd528949a89b35d0bfd383126b08d
Brochu <alexbrochu1@gmail.com>
2023-10-28 01:06:26 -0400

    [DEV] Working on getting the basic smoke shaders to complile

--- src/SmokeRenderPass.cpp
+++ src/SmokeRenderPass.cpp
@@ -59,16 +59,16 @@ void SmokeRenderPass::CreateRootSignature() {
     if (rootSignature_.Get() != nullptr) return;
 
     // We have one parameter: a constant buffer to send debug params to smoke shader
-    CD3DX12_ROOT_PARAMETER parameters[1];
-    parameters[0].InitAsConstantBufferView (0);
+    //CD3DX12_ROOT_PARAMETER parameters[1];
+    //parameters[0].InitAsConstantBufferView (0);
 
     // Maybe we will need a static sample for later?
-    CD3DX12_STATIC_SAMPLER_DESC samplers[1];
-    samplers[0].Init (0, D3D12_FILTER_MIN_MAG_LINEAR_MIP_POINT);
+    //CD3DX12_STATIC_SAMPLER_DESC samplers[1];
+    //samplers[0].Init (0, D3D12_FILTER_MIN_MAG_LINEAR_MIP_POINT);
 
     CD3DX12_ROOT_SIGNATURE_DESC descRootSignature;
-    descRootSignature.Init (1, parameters,
-                            1, samplers,
+    descRootSignature.Init (0, nullptr/*parameters*/,
+                            0, nullptr/*samplers*/,
                             D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT
     );
 
@@ -83,62 +83,50 @@ void SmokeRenderPass::CreateRootSignature() {
 }
 
 void SmokeRenderPass::CreatePipelineStateObject() {
-    //if (pso_.Get() != nullptr) return;
+    if (pso_.Get() != nullptr) return;
 
-    //std::vector<unsigned char> code = ReadFile(shaderFile_);
+    std::vector<unsigned char> code = ReadFile(smokeShader_);
 
-    //static const D3D12_INPUT_ELEMENT_DESC layout[] =
-    //{
-    //    { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,
-    //    D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
-    //    { "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12,
-    //    D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
-    //    { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 24,
-    //    D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
-    //};
+    static const D3D_SHADER_MACRO macros[] = {
+        { nullptr, nullptr }
+    };
 
-    //static const D3D_SHADER_MACRO macros[] = {
-    //    { nullptr, nullptr }
-    //};
+    ComPtr<ID3DBlob> error;
+    ComPtr<ID3DBlob> vertexShader;
+    D3DCompile (code.data(), code.size(),
+        "", macros, nullptr,
+        "VS_main", "vs_5_1", 0, 0, &vertexShader, &error);
+    const char *vserr = reinterpret_cast<const char*>(error->GetBufferPointer());
 
-    //ComPtr<ID3DBlob> error;
-    //ComPtr<ID3DBlob> vertexShader;
-    //D3DCompile (code.data(), code.size(),
-    //    "", macros, nullptr,
-    //    "VS_main", "vs_5_1", 0, 0, &vertexShader, &error);
-    //unsigned char *vserr = reinterpret_cast<unsigned char*>(error->GetBufferPointer());
+    ComPtr<ID3DBlob> pixelShader;
+    D3DCompile (code.data(), code.size(),
+        "", macros, nullptr,
+        "PS_main", "ps_5_1", 0, 0, &pixelShader, &error);
+    unsigned char *pserr = reinterpret_cast<unsigned char*>(error->GetBufferPointer());
 
-    //ComPtr<ID3DBlob> pixelShader;
-    //D3DCompile (code.data(), code.size(),
-    //    "", macros, nullptr,
-    //    "PS_main", "ps_5_1", 0, 0, &pixelShader, &error);
-    //unsigned char *pserr = reinterpret_cast<unsigned char*>(error->GetBufferPointer());
+    // WHY CANT WE HAVE DEBUG SYMBOLS
+    //auto vsize = vertexShader->GetBufferSize();
+    //auto psize = pixelShader->GetBufferSize();
 
-    //D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
-    //psoDesc.VS.BytecodeLength = vertexShader->GetBufferSize ();
-    //psoDesc.VS.pShaderBytecode = vertexShader->GetBufferPointer ();
-    //psoDesc.PS.BytecodeLength = pixelShader->GetBufferSize ();
-    //psoDesc.PS.pShaderBytecode = pixelShader->GetBufferPointer ();
-    //psoDesc.pRootSignature = rootSignature_.Get ();
-    //psoDesc.NumRenderTargets = 1;
-    //psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
-    //psoDesc.DSVFormat = DXGI_FORMAT_D32_FLOAT;
-    //psoDesc.InputLayout.NumElements = std::extent<decltype(layout)>::value;
-    //psoDesc.InputLayout.pInputElementDescs = layout;
-    //psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC (D3D12_DEFAULT);
-    //psoDesc.BlendState = CD3DX12_BLEND_DESC (D3D12_DEFAULT);
-    //// Simple alpha blending
-    ////TODO: I think blending works okay, but the draws are not sorted so looks strange
-    //psoDesc.BlendState.RenderTarget[0].BlendEnable = true;
-    //psoDesc.BlendState.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
-    //psoDesc.BlendState.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
-    //psoDesc.BlendState.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
-    //psoDesc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
-    //psoDesc.SampleDesc.Count = 1;
-    //psoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
-    //psoDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_GREATER;
-    //psoDesc.SampleMask = 0xFFFFFFFF;
-    //psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+    D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = {};
+    psoDesc.InputLayout = {nullptr, 0};
+    psoDesc.pRootSignature = rootSignature_.Get();
+    //psoDesc.VS.BytecodeLength = vertexShader->GetBufferSize();
+    //psoDesc.VS.pShaderBytecode = vertexShader->GetBufferPointer();
+    //psoDesc.PS.BytecodeLength = pixelShader->GetBufferSize();
+    //psoDesc.PS.pShaderBytecode = pixelShader->GetBufferPointer();
+    psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+    psoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;
+    psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+    psoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
+    psoDesc.DepthStencilState.DepthEnable = false;
+    psoDesc.DepthStencilState.StencilEnable = false;
+    psoDesc.SampleMask = UINT_MAX;
+    psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+    psoDesc.NumRenderTargets = 1;
+    psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+    psoDesc.DSVFormat = DXGI_FORMAT_UNKNOWN;
+    psoDesc.SampleDesc.Count = 1;
 
     //device_->CreateGraphicsPipelineState (&psoDesc, IID_PPV_ARGS (&pso_));
 }
